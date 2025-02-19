98068ea34024108e3da82d48bd4980fbc27ef762
Jannis Born <jab@zurich.ibm.com>
2025-02-19 14:52:45 +0100

    debug: GPU check

--- src/ntl/trainer.py
+++ src/ntl/trainer.py
@@ -1003,7 +1003,7 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
             step = -1
             for step, inputs in enumerate(epoch_iterator):
                 if step == -1:
-                    logger.info('inputs', inputs['input_ids'][:2, :10])
+                    logger.info(f'inputs: {inputs['input_ids'][:2, :10]}')
                 total_batched_samples += 1
 
                 if self.args.include_num_input_tokens_seen:
